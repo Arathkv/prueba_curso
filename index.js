d8b4fb51a748358c78746414b5bd7db71fa63411
Arath Koh Velazquez <arath.koh@gmail.com>
2023-12-08 13:25:29 -0600

    ruta hola añadida

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ const port = 3000;
 
 app.use(express.static(__dirname + '/public'));
 
+app.get( '/hola', (req, res) =>{
+    res.send('ruta Hola');
+});
+
+app.get( '/adios', (req, res) =>{
+    res.send('ruta Adios');
+});
+
 app.listen(port, () => {
   console.log('Servidor escuchando en http://localhost:${port}');},);
